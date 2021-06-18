deec23b4c5aeba5e5ba9e033deb7e1acd335c75e
xingyic <cxy1989112@gmail.com>
2021-06-18 11:36:23 +0800

    add multi select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,30 +1,35 @@
-import React, {useState} from 'react';
-import { Select as SelectC, SelectProps as SelectPropsC, Icon, Box,
+import React, { useState, useRef, useEffect } from 'react';
+import {
+    Select as SelectC, SelectProps as SelectPropsC, Icon, Box,
     Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverHeader,
-  PopoverBody,
-  PopoverFooter,
-  PopoverArrow,
-  PopoverCloseButton,
-  VStack
- } from '@chakra-ui/react';
- import {find, propEq} from 'ramda';
+    PopoverTrigger,
+    PopoverContent,
+    PopoverBody,
+    useOutsideClick,
+    Tag,
+    VStack,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    TagLabel,
+    TagCloseButton,
+    Flex
+} from '@chakra-ui/react';
+import { find, propEq, filter, map } from 'ramda';
 import { FaCaretDown } from "react-icons/fa";
-import {Button} from './Button';
-import Downshift from 'downshift'
+import { Button } from './Button';
+import { useMeasure } from 'react-use';
 
-interface SelectProps extends SelectPropsC{
+interface SelectProps extends SelectPropsC {
     items: {
         value: unknown,
         label: string
     }[]
 }
 const Select = ({ select }: SelectProps): JSX.Element => {
-    const {currentSelection, items} = select;
-    const currentElem = find(propEq('value',currentSelection))(items);
-    return (<Button 
+    const { currentSelection, items } = select;
+    const currentElem = find(propEq('value', currentSelection))(items);
+    return (<Button
         border='none'
         _hover={{
             boxShadow: '0 2px 1px -1px var(--chakra-colors-pri-01)',
@@ -33,26 +38,101 @@ const Select = ({ select }: SelectProps): JSX.Element => {
         borderRadius="0.25rem"
         _focus={{
             boxShadow: 'none',
-        }} rightIcon={<Icon as={FaCaretDown}/> }>{currentElem.label}</Button>)
+        }} rightIcon={<Icon as={FaCaretDown} />}>{currentElem.label}</Button>)
 }
 
 
-Select.Option = ({value,label, currentSelection , setSelect }) => {
-    return <VStack   align="stretch"  spacing={4}
-    ><Button  border="none" value={value} variant="secondary" isActive={currentSelection===value} onClick={()=>setSelect(value)}>{label}</Button></VStack>
+Select.Option = ({ value, label, currentSelection, setSelect }) => {
+    return <VStack align="stretch" spacing={4}
+    ><Button border="none" value={value} variant="secondary" isActive={currentSelection === value} onClick={() => setSelect(value)}>{label}</Button></VStack>
 }
 
-export const SelectLayout = ({select, children}) => {
-    return <Popover>
-    <PopoverTrigger>
-        <Box w="fit-content">{children[0](select)}</Box>
-    </PopoverTrigger>
-    <PopoverContent mt="2" _focus={{
-                    boxShadow: 'none',
-                }}>
-      <PopoverBody>{children[1](select)}</PopoverBody>
-    </PopoverContent>
-  </Popover>
+interface multiSelect {
+    select: {
+        items: {
+            value: string | number;
+            label: string;
+        }[];
+        currentSelection: string | number[];
+        filter: string | number;
+    }
+}
+export const MultipleSelect = ({ select, setSelect }: multiSelect) => {
+    const { currentSelection, items, filter: selectFilter } = select;
+    const [isFocus, setIsFocus] = useState(false);
+    const currentRef = useRef()
+    const currentElem = filter(
+        ({ value }) => {
+            return currentSelection.includes(value);
+        }
+        , items
+    );
+
+    return <Flex outline={isFocus ? '1px solid' : "0px solid"} outlineColor="pri.01" w="13rem" p="1"
+        flexWrap="wrap" h="auto"
+        justifyContent="flex-start"
+        onClick={(e) => {
+            e.preventDefault();
+            setIsFocus(true)
+            setSelect({ ...select, filter: "" })
+            currentRef.current.focus();
+        }}
+    >
+        {
+            map(
+                ({ value, label }) => <Tag h="2rem" m="1" key={value}><TagLabel>{label}</TagLabel><TagCloseButton onClick={(e) => {
+                    e.stopPropagation();
+                    setSelect({ ...select, currentSelection: filter((v) => v !== value, currentSelection) })
+                }} /></Tag>
+                , currentElem
+            )
+        }
+        <Flex flex={1}>
+            <Input
+                ref={currentRef}
+                value={selectFilter}
+                onChange={(event) => setSelect({ ...select, filter: event.target.value })}
+                onFocus={
+                    () => setIsFocus(true)
+                }
+                onBlur={
+                    () => {
+                        setSelect({ ...select })
+                        setIsFocus(false);
+                    }
+                }
+                border="none"
+                _focus={{
+                    boxShadow: 'none'
+                }}
+            />
+        </Flex>
+    </Flex>
+}
+MultipleSelect.Option = ({ value, label, currentSelection, setSelect }) => {
+    return <VStack align="stretch" spacing={4}
+    ><Button border="none" value={value} variant="secondary" isActive={currentSelection.includes(value)} onClick={() => setSelect(value)}>{label}</Button></VStack>
+}
+
+export const SelectLayout = ({ select, children, setSelect }) => {
+    const { filter } = select;
+    const initialFocusRef = React.useRef();
+    const insideRef = React.useRef();
+    const [isShow, setIsShow] = useState(false);
+    useOutsideClick({
+        ref: insideRef,
+        handler: () => setIsShow(false)
+    });
+    return <Box ref={insideRef} display="inline-block"><Popover isOpen={isShow} initialFocusRef={initialFocusRef}>
+        <PopoverTrigger>
+            <Box onClick={() => setIsShow(true)} w="fit-content" ref={initialFocusRef} >{children[0](select, setSelect)}</Box>
+        </PopoverTrigger>
+        <PopoverContent mt="2" _focus={{
+            boxShadow: 'none',
+        }}>
+            <PopoverBody >{children[1](select, setSelect)}</PopoverBody>
+        </PopoverContent>
+    </Popover></Box>
 }
 
 export default Select
--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { Story, Meta } from '@storybook/react';
 import { EmailIcon } from '@chakra-ui/icons';
-import Select, { SelectLayout } from '../components/Select';
+import Select, { SelectLayout, MultipleSelect } from '../components/Select';
 import { SelectProps } from '@chakra-ui/react';
 import { Button, ButtonProps } from '../components/Button';
-
-import { map } from 'ramda';
+import { map, uniq, filter } from 'ramda';
 
 export default {
     title: 'Example/Select',
@@ -35,8 +34,37 @@ const Template: Story<SelectProps> = (args) => {
         </SelectLayout>
     )
 };
+const MutliTemplate: Story<SelectProps> = (args) => {
+    const [select, setSelect] = useState({
+        currentSelection: [1,20,3,33,],
+        items: [
+            { value: 1, label: 1 }, { value: 20, label: 20 }, { value: 3, label: 3 },
+            { value: 33, label: 33 }, { value: 10, label: 10 }, { value: 6, label: 6 }
+        ],
+    });
+    return (
+        <SelectLayout select={select}  setSelect={setSelect}>
+            {
+                (selection, setSelection) => <MultipleSelect select={selection} setSelect={setSelection}/>
+            }
+            {
+               (selection, setSelection) => map(({ value, label }) => <MultipleSelect.Option key={value} value={value} label={label} currentSelection={selection.currentSelection} setSelect={(v) => {
+                   console.log(selection.currentSelection.includes(v) ? filter((elem)=>elem!==v, selection.currentSelection) : [v, ...selection.currentSelection])
+                setSelection({
+                    ...selection,
+                    currentSelection: selection.currentSelection.includes(v) ? filter((elem)=>elem!==v, selection.currentSelection) : [v, ...selection.currentSelection]
+                })
+            }} />, filter(({value})=>`${value}`.includes(`${selection.filter}`), selection.items))
+            }
+        </SelectLayout>
+    )
+};
+export const single = Template.bind({});
+single.args = {
 
-export const Primary = Template.bind({});
-Primary.args = {
+};
+
+export const mutli = MutliTemplate.bind({});
+mutli.args = {
 
 };
