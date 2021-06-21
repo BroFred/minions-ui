8cc1a59729e5bbded94017bf6301432209492343
xingyic <cxy1989112@gmail.com>
2021-06-21 20:30:22 +0800

    add theme to monorepo

--- packages/Theme/src/Theme.ts
+++ packages/Theme/src/Theme.ts
@@ -1,8 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 import colors from './color';
 import shadows from './shadow';
-import { ButtonTheme as Button } from '../../packages/Button/src/Button';
-import { paginationTheme as Pagination } from '../../packages/Pagination/src/Pagination';
+import { ButtonTheme as Button } from '@minion-ui/button';
+import { paginationTheme as Pagination } from '@minion-ui/pagination';
+import {map} from 'ramda';
+console.log(map)
 const overrides = {
   colors,
   shadows,
