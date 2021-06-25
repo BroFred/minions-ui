578edaf512a0fba9647b2579ec3397417966bbb6
liuyaqun <liuyaqun@360.cn>
2021-06-25 18:51:04 +0800

    fix modal

--- packages/Modal/src/Modal.tsx
+++ packages/Modal/src/Modal.tsx
@@ -40,7 +40,7 @@ export const Modal:React.FC<ModalProps> = ({
 }) => {
   const { colorMode } = useColorMode();
   const {content: confirmContent, callBack: confirmCallBack, ...otherConfirm} = confirmBtn;
-  const {content: cancelContent, callBack: cancelCallBack, ...otherCancelBtn} = cancelBtn;
+  const {content: cancelContent, callBack: cancelCallBack, ...otherCancelBtn} = cancelBtn || {};
   const getJustify = () => {
     return extraFootInfo ? 'space-between ': 'flex-end';
   }
@@ -66,7 +66,7 @@ export const Modal:React.FC<ModalProps> = ({
         <ModalFooter borderRadius='0.5rem' bg={`${colorMode === 'light' ? 'nl.10' : 'nl.10'}`} mt='1rem' px='2rem' py='0.75rem' justifyContent={getJustify()}>
           {extraFootInfo}
           <div>
-            {!!cancelBtn && <Button mode='secondary' onClick={cancelCallBack} mr='0.5rem' {...otherConfirm}>{cancelContent}</Button>}
+            {!!cancelContent && <Button mode='secondary' onClick={cancelCallBack} mr='0.5rem' {...otherConfirm}>{cancelContent}</Button>}
             <Button onClick={confirmCallBack} {...otherCancelBtn}>{confirmContent}</Button>
           </div>
         </ModalFooter>
