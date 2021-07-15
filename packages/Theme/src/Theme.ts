06c7e0ab233db3cb8d1f86539f4a813f7fcf9274
liuyaqun <liuyaqun@360.cn>
2021-07-15 18:00:25 +0800

    feat: add breakpoints

--- packages/Theme/src/Theme.ts
+++ packages/Theme/src/Theme.ts
@@ -3,12 +3,20 @@ import colors from './color';
 import shadows from './shadow';
 import { ButtonTheme as Button } from '@minion-ui/button';
 import { paginationTheme as Pagination } from '@minion-ui/pagination';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 import { always } from 'ramda';
 
 always(undefined);
+const breakpoints = createBreakpoints({
+  sm: '920px',
+  md: '1440px',
+  lg: '1920px',
+  xl: '2326px',
+})
 const overrides = {
   colors,
   shadows,
+  breakpoints: breakpoints,
   components: {
     Button,
     Pagination,
