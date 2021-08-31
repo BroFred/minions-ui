16ead601cb8d2dffe2c53c9d5aaedf70c14dda7f
lijunxin <lijunxin@360.cn>
2021-08-31 10:06:15 +0800

    添加drag drop story

--- packages/SqlEditor/stories/SqlEditor.stories.tsx
+++ packages/SqlEditor/stories/SqlEditor.stories.tsx
@@ -9,6 +9,16 @@ const SqlEditorTemplate: Story = () => {
     const [currentValue, setCurrentValue] = useState<string>('');
     console.log('story', currentValue);
     const sqlTables = useMemo(() => zipObj(clone(tableList).sort().map((item) => item.name), clone(tableList).sort().map((item) => item.data.map((column) => column.name))), [tableList]);
+    return <><SqlEditor value='' autoComplete={true} tables={sqlTables} tableName='price' wrapClassname='junior' callback={setCurrentValue} />
+        <p>{currentValue}</p>
+        <div>123456</div>
+    </>
+}
+
+const SqlEditorDragDropTemplate: Story = () => {
+    const [currentValue, setCurrentValue] = useState<string>('');
+    console.log('story', currentValue);
+    const sqlTables = useMemo(() => zipObj(clone(tableList).sort().map((item) => item.name), clone(tableList).sort().map((item) => item.data.map((column) => column.name))), [tableList]);
     return <><SqlEditor value='' autoComplete={true} tables={{}} tableName='price' wrapClassname='junior' callback={setCurrentValue} />
         <p>{currentValue}</p>
         <div id='123456' draggable
@@ -17,16 +27,18 @@ const SqlEditorTemplate: Story = () => {
                 ev.dataTransfer.setData('text', '爷来啦');
                 // ev.currentTarget.style.border = 'dashed';
                 ev.dataTransfer.dropEffect = 'copy';
-                ev.effectAllowed = 'copyMove';
+                // ev.effectAllowed = 'copyMove';
               }}
               onDragEnd={(ev) => {
                 console.log('dragEnd');
                 ev.dataTransfer.clearData();
-              }}>123456</div>
+              }}>Drag Me</div>
     </>
 }
 
-export const SqlEditorStory = SqlEditorTemplate.bind({})
+export const SqlEditorStory = SqlEditorTemplate.bind({});
+
+export const SqlEditorDragDropStory = SqlEditorDragDropTemplate.bind({});
 
 export default {
     title: 'SqlEditor',
